1cfe77cacb193224a395bc87b41b0eed527fbf44
Jonathan Cumini <jonathancumini@gmail.com>
2023-05-02 13:46:27 -0300

    First big commit
    Runner movement
    Camera follow
    Collision
    
    Few obstacles and running through a long but limited ground

---
+++ Assets/RunnerCollision.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class RunnerCollision : MonoBehaviour {
+
+	public RunnerMovement movement;
+
+	void OnCollisionEnter(Collision collisionInfo)
+	{
+		string colliderTag = collisionInfo.collider.tag;
+
+		if(colliderTag.Equals("Obstacle"))
+		{
+			movement.enabled = false;
+		}
+
+
+	}
+}
---
+++ Assets/RunnerMovement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class RunnerMovement : MonoBehaviour {
+
+	// A Rigidbody is needed to apply gravity to the game object, for example. 
+	// In Unity, drag the Rigidbody object to this (look for its name in the Script section of the Inspector)
+	public Rigidbody RunnerRigidbody = new Rigidbody(); 
+	public float forwardForce = 2000f;
+	public float sidewaysForce = 500f;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("Game starting...");		
+		}
+		
+	// FixedUpdate > Update() when working with physics in the inside (forces, velocity, etc.)
+	void FixedUpdate () {		
+		RunnerRigidbody.AddForce(0, 0, forwardForce * Time.deltaTime); // This force needs to be applied continously for our purpose
+
+		if(Input.GetKey("d"))
+		{
+			RunnerRigidbody.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+		}
+
+		if(Input.GetKey("a"))
+		{
+			RunnerRigidbody.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+		}
+	}
+}
