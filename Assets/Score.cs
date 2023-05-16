dbb660959cdab93d1e6e0796f77dc16aa98e98c9
Jonathan Cumini <jonathancumini@gmail.com>
2023-05-16 10:39:42 -0300

    Score added

---
+++ Assets/Score.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+
+	public Transform runner;
+	public Text scoreText;
+
+	// Update is called once per frame
+	void Update () {
+		scoreText.text = runner.position.z.ToString("0");		
+	}
+}
